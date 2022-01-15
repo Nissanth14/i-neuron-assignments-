d64427ec24ff655729f07a81bf312c59ce8b9ccb
Nissanth <96663147+Nissanth14@users.noreply.github.com>
2022-01-16 00:32:23 +0530

    Add files via upload

---
+++ assignment3.py.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ##### Q1
+# 
+# What will the output be of ((500//7) % 5) ** 3?
+
+# ##### Q2
+# 
+# How will you extract ‘love’ from the string S = “I love Python”? (More than one option may be correct.).
+
+# ##### Q3
+# 
+# What will the output of 3 * 3 ** 3 be?
+
+# ##### Q4
+# 
+# If you have a tuple T = (3, 5, 7, 11), what will the output of T.append(9) be?
+
+# ##### Q5
+# 
+# What will the output of the following program be?
+# 
+# L1 = ['Vikas', 'Akshay', 'Sanskar', 'Mahima']
+# 
+# print(L1[1][-1])
+
+# ##### Q6
+# 
+# What will be the output ?
+# 
+# l = [32, 34, 12, 27, 33]
+# 
+# l.append([14, 19])
+# 
+# print(len(l))
+
+# In[ ]:
+
+
+1
+
+
+# In[ ]:
+
+
+s[2:6]
+
+
+# In[ ]:
+
+
+729
+
+
+# In[ ]:
+
+
+error
+
+
+# In[ ]:
+
+
+vikas,mahima
+
+
+# In[ ]:
+
+
+32,34,12,27,33[14,29]
+5
+
